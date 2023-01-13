7859e359225e0db65e00c61f885f43d89f7e9e25
Ajmin Pradhan <63228592+mdajminpradhan@users.noreply.github.com>
2023-01-13 15:06:51 +0600

    fix eslint issues

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // importing pages
-import Home from "./pages/Home";
+import Home from './pages/Home';
 
 const AppRoutes = () => {
   return (
