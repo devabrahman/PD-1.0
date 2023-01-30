42797635492054f19d3339ba9f0e26f9980381f0
rahman <rahmandevbd@gmail.com>
2023-01-31 02:15:30 +0600

    create New Edit Products pages

---
+++ src/pages/product/NewProducts.js
@@ -0,0 +1,12 @@
+import Breadcrumbs from 'components/common/Breadcrumbs';
+import Layout from 'components/common/Layout';
+
+const NewProducts = () => {
+  return (
+    <Layout title="Products">
+      <Breadcrumbs rootTitle="Products" subTitle="New" />
+    </Layout>
+  );
+};
+
+export default NewProducts;
