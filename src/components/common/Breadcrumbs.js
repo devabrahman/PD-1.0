973a86ca2b6972b9603a92f73174869d91ce85dd
rahman <rahmandevbd@gmail.com>
2023-01-31 02:14:07 +0600

    add breadcrumbs title

--- src/components/common/Breadcrumbs.js
+++ src/components/common/Breadcrumbs.js
@@ -1,16 +1,22 @@
 import ProductDirectoryIcon from 'components/SVGIcons/ProductDirectoryIcon';
+import PropTypes from 'prop-types';
 import { HiChevronRight } from 'react-icons/hi';
 
-const Breadcrumbs = () => {
+const Breadcrumbs = ({ rootTitle, subTitle }) => {
   return (
     <div className="flex flex-row px-14">
       <ProductDirectoryIcon className="text-[#4F4F4F] border-red-200" />
       <HiChevronRight className="h-7 w-10 text-[#D9D9D9]" />
-      <p>Producst</p>
+      <p>{rootTitle}</p>
       <HiChevronRight className="h-7 w-10 text-[#D9D9D9]" />
-      <p className="text-[#1C92FF]">Product Details</p>
+      <p className="text-[#1C92FF]">{subTitle}</p>
     </div>
   );
 };
 
 export default Breadcrumbs;
+
+Breadcrumbs.propTypes = {
+  rootTitle: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired
+};
