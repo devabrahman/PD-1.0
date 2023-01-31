b64de08038a1461eb147b5c690c81a981fa744a5
rahman <rahmandevbd@gmail.com>
2023-02-01 00:05:09 +0600

    add custom font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       colors: {
         green: '#2CF672',
         gold: '#FFCF0D',
-        primary: '#5798F6'
+        primary: '#5798F6',
+        pdGrey: '#D6D6D6',
+        accent: ' #1C92FF'
+      },
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif']
       }
     }
   },
