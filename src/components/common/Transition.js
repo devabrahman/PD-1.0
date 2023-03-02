cf916240302bb455e8ccb35f4be2f30ee90b7008
sumon Islam <rahmandevbd@gmail.com>
2023-03-02 12:43:00 +0600

    Add transition as a compnent

---
+++ src/components/common/Transition.js
@@ -0,0 +1,15 @@
+function Transition({ children }) {
+  return (
+    <Transition
+      enter="transition duration-200 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-175 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0">
+      {children}
+    </Transition>
+  );
+}
+
+export default Transition;
