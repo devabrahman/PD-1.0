400d7f5087108b69752f1f3522609d0221105474
sumon Islam <rahmandevbd@gmail.com>
2023-03-02 12:43:57 +0600

    replace to top option

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -16,15 +16,13 @@ const Layout = ({ title, children }) => {
         <div className="flex justify-between items-center pr-14 mt-8">
           <h2 className="text-[27px] font-medium">{title}</h2>
 
-          <div className="flex items-center">
+          <div className="flex items-center gap-6">
             <MagnifyingGlassIcon className="bg-white transform-gpu hover:scale-95 transition-all duration-150 text-blue-400 border border-gray-50 shadow-lg shadow-blue-100 h-10 w-12 rounded-xl px-2.5 cursor-pointer" />
-            <div className="relative bg-white transform-gpu hover:scale-95 transition-all duration-150 border border-gray-50 shadow-lg shadow-blue-100 rounded-xl grid place-content-center cursor-pointer ml-6">
+            <div className="relative bg-white transform-gpu hover:scale-95 transition-all duration-150 border border-gray-50 shadow-lg shadow-blue-100 rounded-xl grid place-content-center cursor-pointer">
               <BellAlertIcon className="h-10 w-12 text-blue-400 px-2.5" />
               <span className="h-2 w-2 rounded-full bg-red-400 absolute right-1 top-1"></span>
             </div>
-            <div className="relative bg-white transform-gpu hover:scale-95 transition-all duration-150 border border-gray-50 shadow-lg shadow-blue-100 rounded-xl grid place-content-center cursor-pointer ml-6 h-10 w-12 px-2">
-              <TopOptions />
-            </div>
+            <TopOptions />
             <ProfileDialog />
           </div>
         </div>
