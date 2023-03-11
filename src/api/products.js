461c73528ed2cce61e384621bd04bbb0ef334787
sumon Islam <rahmandevbd@gmail.com>
2023-03-11 23:13:20 +0600

    get data

---
+++ src/api/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    sales: 198,
+    month: 'Jan'
+  },
+  {
+    sales: 106,
+    month: 'Feb'
+  },
+  {
+    sales: 261,
+    month: 'Mar'
+  },
+  {
+    sales: 194,
+    month: 'App'
+  },
+  {
+    sales: 318,
+    month: 'May'
+  },
+  {
+    sales: 138,
+    month: 'Jun'
+  }
+];
---
+++ src/api/spent.js
@@ -0,0 +1,32 @@
+export const spent = [
+  {
+    price: 307,
+    price2: 292,
+    time: 'sep'
+  },
+  {
+    price: 301,
+    price2: 209,
+    time: 'oct'
+  },
+  {
+    price: 497,
+    price2: 276,
+    time: 'nov'
+  },
+  {
+    price: 302,
+    price2: 102,
+    time: 'dev'
+  },
+  {
+    price: 281,
+    price2: 204,
+    time: 'jan'
+  },
+  {
+    price: 455,
+    price2: 264,
+    time: 'feb'
+  }
+];
