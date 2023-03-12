cf865605321162a35c9a3cd7da5b546eeade4b79
sumon Islam <rahmandevbd@gmail.com>
2023-03-12 16:47:24 +0600

    change darkmode theme

--- src/components/profile/ChangeThemes.js
+++ src/components/profile/ChangeThemes.js
@@ -3,23 +3,31 @@ import { MoonIcon, SunIcon, StarIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { setDarkMode, themeCheck } from 'utils/darkmode';
 
 function ChangeThemes({ className }) {
   const [selectedIndex, setSelectedIndex] = useState(0);
-
+  // const activeTheme = themeCheck();
   const applyTheme = (name) => {
-    console.log('🔐 -> file: ChangeThemes.js:7 -> applyTheme -> name:', name);
+    setDarkMode(name);
   };
 
   useEffect(() => {
-    console.log('🔐 -> file: ChangeThemes.js:15 -> ChangeThemes -> selectedIndex:', selectedIndex);
-  }, [selectedIndex]);
+    const activeTheme = themeCheck();
+    console.log('🔐 -> file: ChangeThemes.js:17 -> useEffect -> activeTheme:', activeTheme);
+
+    if (activeTheme === 'light') {
+      setSelectedIndex(0);
+    } else if (activeTheme === 'dark') {
+      setSelectedIndex(1);
+    }
+  }, []);
 
   return (
     <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-      <Tab.List className={`flex gap-1 bg-[#F4F4F8] p-1 rounded-lg ${className}`}>
+      <Tab.List className={`flex gap-1 bg-[#F4F4F8] dark:bg-[#211F26] p-1 rounded-lg ${className}`}>
         <Tab
-          className={`flex gap-2 bg text-base items-center ${
+          className={`dark:bg-transparent flex gap-2 bg text-base items-center ${
             selectedIndex === 0 && 'bg-white'
           }  rounded-lg py-1.5 px-2.5`}
           onClick={() => applyTheme('light')}>
@@ -27,11 +35,16 @@ function ChangeThemes({ className }) {
         </Tab>
 
         <Tab
-          className={`flex gap-2 bg text-base items-center ${
-            selectedIndex === 1 && 'bg-white'
+          className={`flex gap-2 dark:text-white bg text-base items-center ${
+            selectedIndex === 1 && 'bg-[#474950]'
           }  rounded-lg py-1.5 px-2.5`}
           onClick={() => applyTheme('dark')}>
-          <MoonIcon className={`w-[20px] ${selectedIndex === 1 && 'text-gold'}`} /> Dark
+          <MoonIcon
+            className={`w-[20px] ${
+              selectedIndex === 1 && 'text-white dark:bg-[#474950] fill-white'
+            }`}
+          />{' '}
+          Dark
         </Tab>
 
         <Tab
