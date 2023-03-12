b90eb82e92d9478c8270c9519040c99d331af58e
sumon Islam <rahmandevbd@gmail.com>
2023-03-12 16:47:36 +0600

    add darmode support

--- src/components/profile/ProfileSetting.js
+++ src/components/profile/ProfileSetting.js
@@ -18,9 +18,9 @@ const ProfileSetting = () => {
       </Menu.Button>
 
       <TransitionComponent>
-        <Menu.Items className="absolute -right-3 z-10 w-80 top-3  bg-white input-accent-box-shadow py-6 px-5 rounded-3xl">
+        <Menu.Items className="absolute -right-3 z-10 w-80 top-3 dark:bg-[#303038] dark:text-white  bg-white input-accent-box-shadow py-6 px-5 rounded-3xl">
           <Menu.Item>
-            <h5 className="text-[#302323]  font-medium">Theme</h5>
+            <h5 className="text-[#302323] dark:text-white font-medium">Theme</h5>
           </Menu.Item>
           <Menu.Item>
             <ChangeThemes className="mt-6 mb-7" />
