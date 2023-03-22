91dd474ea3f738695a52a153eec999179751ee40
sumon Islam <rahmandevbd@gmail.com>
2023-03-23 01:19:37 +0600

    fix tags

--- src/components/tags/NewTag.js
+++ src/components/tags/NewTag.js
@@ -1,20 +1,19 @@
 import { Dialog } from '@headlessui/react';
 import { BsXLg } from 'react-icons/bs';
-import CategoryPic from '../../Images/CategoryPic.png';
+import CategoryPic from 'assets/Images/common/CategoryPic.png';
 
 const NewTag = ({ isOpen, setIsOpen }) => {
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
-      {/* The backdrop, rendered as a fixed sibling to the panel container */}
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-      {/* Full-screen container to center the panel */}
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        {/* The actual dialog panel  */}
         <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6  shadow-xl transition-all text-center">
           <div className="mx-0 my-0">
-            <section className="flex justify-end">
-              <BsXLg className="w-6 text-[#C8C9CB]" />{' '}
+            <section
+              onClick={() => setIsOpen(false)}
+              className="flex justify-end group-hover cursor-pointer">
+              <BsXLg className="w-6 text-[#C8C9CB] hover:text-[#F4316B] duration-200" />
             </section>
             <section className="flex mb-16">
               <img src={CategoryPic} className="w-60 h-44" />
@@ -25,7 +24,7 @@ const NewTag = ({ isOpen, setIsOpen }) => {
                   </label>
                   <input
                     type="text"
-                    className="rounded-xl flex justify-center items-center border border-primary bg-primary bg-opacity-10 p-4 font-montserrat text-sm w-full"
+                    className="rounded-xl flex justify-center items-center border border-[#B9B9B9]/50  p-4 font-montserrat text-sm w-full input-box-shadow"
                     required
                     placeholder="Tags Name"
                   />
@@ -37,7 +36,8 @@ const NewTag = ({ isOpen, setIsOpen }) => {
                   <textarea
                     type="text"
                     rows="5"
-                    className="rounded-xl flex justify-center items-center border border-primary bg-primary bg-opacity-10 py-2  px-2 font-montserrat text-sm w-full"
+                    className="rounded-xl flex justify-center items-center border border-[#B9B9B9]/50
+                     py-2  px-2 font-montserrat text-sm w-full outline-none"
                     required
                     placeholder="Tags Name"
                   />
