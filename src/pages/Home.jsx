3a968911a18902eed38fe58ce1726a7d4d48f5ae
Ajmin Pradhan <63228592+mdajminpradhan@users.noreply.github.com>
2023-04-14 15:03:22 +0600

    title, colors, component, design changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { CheckIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import { CalendarDaysIcon, CheckIcon, ChevronUpIcon, TruckIcon } from '@heroicons/react/24/outline';
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 import { spent } from 'api/spent';
 import { Overview } from 'components/dashboard/Overview';
@@ -8,13 +8,13 @@ import Activity from 'components/dashboard/Activity';
 import Payment from 'components/dashboard/Payment';
 import TotalProduct from 'components/dashboard/TotalProduct';
 import TotalSales from 'components/dashboard/TotalSales';
+
 // icon
 import ChartIcon from 'components/SVGIcons/ChartIcon';
 import DollarIcon from 'components/SVGIcons/DollarIcon';
 import ReportIcon from 'components/SVGIcons/ReportIcon';
 import WalletIcon from 'components/SVGIcons/WalletIcon';
 import map from 'assets/Images/common/Map.png';
-import CalenderIcon from 'components/SVGIcons/CalenderIcon';
 
 const Home = () => {
   // Get Data from demo api
@@ -22,12 +22,12 @@ const Home = () => {
 
   return (
     // TODO
-    <Layout title="Products" padding="px-8 py-8">
+    <Layout title="Dashboard" padding="px-8 py-8">
       <main className="py-5 dark:bg-dark px-3 sm:px-0">
         <section>
-          <div className="grid grid-cols-12 gap-x-[18px] gap-y-[18px]">
+          <div className="grid grid-cols-12 gap-x-6 gap-y-[18px]">
             <div className="col-span-6 sm:col-span-3">
-              <Overview icon={<ReportIcon />} title={'Earnings'} amount={350.4} />
+              <Overview icon={<ReportIcon className="" />} title={'Earnings'} amount={350.4} />
             </div>
             <div className="col-span-6 sm:col-span-3">
               <Overview icon={<DollarIcon />} title={'Spend this month'} amount={642.39} />
@@ -35,26 +35,20 @@ const Home = () => {
             <div className="col-span-6 sm:col-span-3 h-auto">
               <Overview icon={<WalletIcon />} title={'Earnings'} amount={350.4} />
             </div>
-            <div className="col-span-6 sm:col-span-3 ">
-              <div className="flex w-full gap-[18px] py-5 px-6 rounded-2xl bg-white dark:bg-dark input-accent-box-shadow">
-                <div className="w-6 h-14  rounded-full flex justify-center items-center"></div>
-                <div>
-                  <p className="font-medium text-[#97A6D5] text-sm">Sales</p>
-                  <p className="font-DMSons font-bold text-lg text-[#2B3674]">$574.34</p>
-                  <p className="text-[#A3AED0] text-[12px] font-bold font-DMSons">
-                    <span className="text-[#05CD99] font-bold">+23% </span>
-                    since last month
-                  </p>
-                </div>
-              </div>
+            <div className="col-span-6 sm:col-span-3 h-auto">
+              <Overview
+                icon={<TruckIcon className="h-7 w-7 stroke-amber-500" />}
+                title={'Deliberies'}
+                amount={350.4}
+              />
             </div>
           </div>
         </section>
-        <section className="grid grid-cols-12 gap-6 mt-11">
-          <div className="col-span-12 sm:col-span-9  mx-7 my-6">
+        <section className="grid grid-cols-12 gap-6 mt-8">
+          <div className="col-span-12 sm:col-span-9 bg-white p-5 rounded-xl">
             <div className="flex justify-between mb-7">
-              <div className="bg-slate-100 rounded-md flex gap-1.5 items-center px-2.5">
-                <CalenderIcon className="stroke-[#A3AED0]" />
+              <div className="rounded-md flex gap-1.5 items-center ">
+                <CalendarDaysIcon className="h-5 w-5 stroke-blue-400" />
                 <p className="text-[#A3AED0] text-[14px] font-medium">This month</p>
               </div>
               <div className="p-1.5 rounded-[10px] bg-[#F4F7FE]">
