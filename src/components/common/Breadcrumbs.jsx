c45422a19f71a14e50ac9789b5d414874c638888
abdur rahman <rahmandevbd@gmail.com>
2023-05-17 00:45:39 +0600

    fix breadcrumb i> home icon color

--- src/components/common/Breadcrumbs.jsx
+++ src/components/common/Breadcrumbs.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Breadcrumbs = ({ title, link }) => {
   return (
     <div className={`flex items-center  dark:text-white`}>
-      <HomeIcon className="bg-white dark:bg-dark dark:text-gray-100 text-gray-400 p-1.5 h-9 w-9 rounded-lg cursor-pointer shadow-lg shadow-blue-50 dark:shadow-gray-700" />
+      <HomeIcon className="bg-white dark:bg-dark dark:text-gray-100 text-gray-400 p-1.5 h-9 w-9 rounded-lg cursor-pointer shadow-lg shadow-blue-50 dark:shadow-gray-800" />
       <ChevronRightIcon className="h-5 w-5 stroke-[2.5] ml-2 text-gray-300" />
 
       <Link to={link} className="ml-2 text-blue-400">
